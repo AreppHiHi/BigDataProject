9f9e1c641463205f41a1853e89630e75eef01327
AreppHiHi <ariffzakwan64@gmail.com>
2025-01-31 22:32:10 +0800

    Add files via upload

---
+++ MallCustomerDriver.java
@@ -0,0 +1,28 @@
+package com.mallcustomer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class MallCustomerDriver {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Customer Segmentation");
+
+        job.setJarByClass(MallCustomerDriver.class);
+        job.setMapperClass(MallCustomerMapper.class);
+        job.setReducerClass(MallCustomerReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
